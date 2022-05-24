fc2e95cb1c0f9e6a314ef9bd68ead546ca9dbc84
Eugene <89818121788@mail.ru>
2022-05-24 19:25:59 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -6,34 +8,35 @@ public class Main {
     }
 
     public static void task1() {
-        int i = 1;
-        while (i <= 10) {
-            System.out.print(i + " ");
-            i++;
-        }
+        int i = 0;
+        while (i < 10) {
+            System.out.print(++i + " ");
+            }
         System.out.println();
-        for (i = 10; i > 0; i--) {
+        for (; i > 0; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
 
         public static void task2 () {
-        int firstFriday = 2;
-        while (firstFriday <= 31) {
-            System.out.println(firstFriday + "-е число. Необходимо подготовить отчет!");
-            firstFriday = firstFriday + 7;
+        int firstFriday = 5;
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.println(currentFriday + "-е число. Необходимо подготовить отчет!");
         }
         }
 
       public  static void task3() {
-        int firstYear = 1822;
-        int lastYear = 2122;
-        for (int i = firstYear; i <= lastYear; i++) {
-            if (i % 79 == 0) {
-                System.out.println(i);
-            }
+        int currentYear = LocalDate.now().getYear();
+        int firstYear = currentYear - 200;
+        int lastYear = currentYear + 100;
+        //int year = firstYear;
+        for (int year = firstYear; year <= lastYear; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }// break;
         }
+        //  System.out.println(year);
          }
 
     }
